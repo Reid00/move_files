dfd03d3aaafb0f9940329978bad93625af824bb2
Reid00 <isreid.zhang@outlook.com>
2019-05-22 22:45:20 +0800

    add more pandas function or method

--- txt_read.py
+++ txt_read.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 import re
+import urllib.parse
 
 
 class ReadCsv:
-
-    def __init__(self, path):
+    def __init__(self, path=None):
         self.path = path
 
     def read_text(self):  # 读取csv或者txt，urls.txt 文件的第二列内容，用常规方法
@@ -35,22 +35,49 @@ class ReadCsv:
         df['new_sk'] = new_sk
         df.to_csv('airport_upper.csv', columns=['SatoriID', 'new_sk'], index=False, header=None)
 
-	def read_url_from_csv():
-    chunksize = 40000
-    reader = pd.read_csv('need_check_urls.tsv', sep='\t', header=None, encoding='utf-8', chunksize=chunksize,
-                         usecols=[2],
-                         names=['urls'])
-    i = 0
-    for chunk in reader:
-        url_decode = [urllib.parse.unquote(str(url)).strip('"') for url in chunk['urls']]
-        chunk['url_decode'] = url_decode
-        # chunk.shape[0] row number
-        i = i + 1
-        chunk.to_csv('url_{}.tsv'.format(i), index=False, header=False, columns=['url_decode'])
-		
+    def read_csv_by_chunk(self):  # 分块读取，写入csv某一列, need_check_urls.tsv
+        chunksize = 40000
+        reader = pd.read_csv(self.path, sep='\t', header=None, encoding='utf-8', chunksize=chunksize,
+                             usecols=[2],
+                             names=['urls'])
+        i = 0
+        for chunk in reader:
+            url_decode = [urllib.parse.unquote(str(url)).strip('"') for url in chunk['urls']]
+            chunk['url_decode'] = url_decode
+            # chunk.shape[0] row number
+            i = i + 1
+            chunk.to_csv('./output/url_{}.tsv'.format(i), index=False, header=None, columns=['url_decode'])
+
+    def compare_content(self, path1, path2):
+        file_1 = pd.read_csv(path1, sep='\t', header=None, usecols=[0], names=['book_id'])
+        file_2 = pd.read_csv(path2, sep='\t', header=None, usecols=[0], names=['book_id'])
+        pattern = re.compile(r'(\d+)')
+        file_1['id'] = [re.search(pattern, book_id).group(1).strip() for book_id in file_1['book_id']]
+        file_2['id'] = [re.search(pattern, book_id).group(1).strip() for book_id in file_2['book_id']]
+        # file_1.to_csv('./output/factbase.csv', index=False, header=None, columns=['id'])
+        # file_2.to_csv('./output/view.csv', index=False, header=None, columns=['id'])
+        df1 = pd.read_csv('./output/1.txt', sep='\t', names=['id', 'name', 'age'])
+        df2 = pd.read_csv('./output/2.txt', sep='\t', names=['id', 'name'])
+        # print([df1['id'] > 0])  # suggest use this method to get columns
+        # print([df1.id > 0])  # not suggest get the column
+        # print(df1[df1['id'] > 0])  # use this method filter some data
+        print(df1[df1['id'] > 0][['name', 'age']])  # use this method filter columns
+        # print(df1[df1['id'] > 0]['name', 'age'])  # this method is wrong
+        df4 = df1[~df1['id'].isin(df2['id'])]  # filter column  is not exsit in another DataFrame
+        df5 = df1[df1['id'].isin(df2['id'])]  # filter column  exsit in another DataFrame
+        df4.to_csv('./output/not_in.csv', index=False, header=True, columns=['id'])
+        df5.to_csv('./output/in.csv', index=False, header=True, columns=['id'])
+        df3 = pd.merge(df1, df2, how='outer', on='id')
+        df3.to_csv('./output/outer_join.csv', index=False, header=True, )
+        not_in_1 = df3.drop(df2.index)
+        # print('not in \n', not_in_1)
+        not_in_1.to_csv('./output/not_in_1.csv', index=False, header=True, columns=['id', 'name_x', 'name_y'])
+
+
 if __name__ == '__main__':
     # text = ReadCsv('url.txt')
     # text.read_text()
     # text.read_text_by_pd()
     csv_reader = ReadCsv('airport.csv')
-    csv_reader.read_csv()
+    csv_reader.compare_content(r'D:\v-baoz\python\dirs_files\input\urls_from_fact_base.tsv',
+                               r'D:\v-baoz\python\dirs_files\input\urls_from_view.tsv')
